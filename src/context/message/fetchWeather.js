84921f74b62cb9dce5e6249d69904a77565b69ea
Andrew <an.smodlev@gmail.com>
2020-02-19 11:46:33 +0300

    -

--- src/context/message/fetchWeather.js
+++ src/context/message/fetchWeather.js
@@ -3,9 +3,6 @@ export default async function fetchWeather () {
     const url = "https://api.darksky.net/forecast/c71f8a6b4970b5ae6916166dfb43ae14/55.755826,37.6173?lang=ru";
     const res = await fetch(proxyurl+url);
     const data = await res.json();
-
-    let dateToday = new Date();
-    let dayOfTheWeek = dateToday.getDay()
-
-    return data.daily.data[dayOfTheWeek+1];
+    //return data[1] because data[0]=today
+    return data.daily.data[1];
 }
